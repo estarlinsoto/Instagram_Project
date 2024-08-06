0473dd50958cb36f87edcd07f603efd228a5d068
estarlinsoto <estarlin-10d@hotmail.com>
2024-08-05 22:05:15 -0400

    endpoints for register and login created

--- src/api/models.py
+++ src/api/models.py
@@ -1,21 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ARRAY
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    avatar = db.Column(db.String(160), unique=False, nullable=False)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    surname = db.Column(db.String(20), unique=False, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+
+   
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            "name": self.name
-            # do not serialize the password, its a security breach
+            "avatar": self.avatar,
+            "name": self.name,
+            "surname": self.surname,
+            "username": self.username
+
         }
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image  = db.Column(db.String(160), unique=False, nullable=False)
+    message = db.Column(db.String(500), unique=False, nullable=False)
+    likes = db.Column(ARRAY(db.String), nullable=True)
+    author = db.Column(db.String(50), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    location = db.Column(db.String(30), unique=True, nullable=False)
+    status = db.Column(db.String(50), unique=True, nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image": self.image,
+            "message": self.message,
+            "likes": self.likes,
+            "author": self.author,
+            "created_at": self.created_at,
+            "location": self.location,
+            "status": self.status
+
+
+        }
+
+
+
+    def __repr__(self):
+        return f'<User {self.name}>'
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,84 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Post
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+app = Flask(__name__)
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+app.config['JWT_SECRET_KEY'] = 'password'  
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    name = request.json.get('name', None)
+    surname = request.json.get('surname', None)
+    avatar = request.json.get('avatar', None)
+
+    if not username or not password or not name or not surname or not avatar:
+        return jsonify({"msg": "Missing user information"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({"msg": "Username already exists"}), 409
+
+    raw_password = password
+    password_hash = bcrypt.generate_password_hash(raw_password).decode('utf-8')
+    new_user = User(
+        username=username,
+        password=password_hash,
+        name=name,
+        surname=surname,
+        avatar=avatar,
+        
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = create_access_token(identity=username)
+    return jsonify({
+       "access_token": access_token,
+       "MSG": "user created!" }), 201
+
+@api.route('/login', methods=['POST'])
+def get_token():
+    
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+
+        if not username or not password:
+            return jsonify({'error': 'usernae and password are required.'}), 400
+
+        username_from_db = User.query.filter_by(username= username).first()
+
+        if not username_from_db:
+            return jsonify({"msg": "username don't exist"}), 404
+
+       
+        password_from_db = username_from_db.password 
+        password_verification = bcrypt.check_password_hash(password_from_db, password) 
+
+        if not password_verification:
+             return jsonify({"msg": "Incorrect password"}), 401
+        
+        else: 
+
+            access_token = create_access_token(identity= username_from_db.id)
+            return jsonify({
+                'access_token': access_token,
+                'msg': 'success'
+                }), 200 
